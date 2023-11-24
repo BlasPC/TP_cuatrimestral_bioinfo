86f7afbaf8fe161850533cbdf875c94c0490acd2
Martina Nudelman <mnudelman@itba.edu.ar>
2023-11-24 13:17:19 -0300

    ej4

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Define input_file con el archivo que contiene la secuencia de nucleotidos de nuestro Ej. 1
-input_file="nucleotidos.fasta"
+# Define input_file con el archivo que contiene la secuencia de nucleotidos de nuestro Ej. 1 (ya debe estar descargado, se descarga en el codigo del Ej 1)
+input_file="nucleotidos.fasta" 
 # Nombre del archivo de salida
 output_file="resultados_prosite.txt"
 
-# Calcula los ORF y obtiene las secuencias de proteínas
-transeq -sequence $input_file -outseq protein_sequences.fasta
+# Define input_fasta donde se va a guardar la secuencia de proteinas traducidas
+input_fasta="protein_sequences.fasta"
 
-# Define input_fasta donde se va a guardar la secuencia de proteinas traducida por EMBOSS con el cual va a realizar el análisis de dominios
-input_fasta="proteins_sequences.fasta"
+# Calcula los 6 ORFs correspondientes y obtiene las secuencias de proteínas
+transeq -sequence $input_file -outseq protein_sequences.fasta -frame 6
 
+# Muesta secuencia de aminoacidos obtenida a partir de la secuencia de nucleotidos ingresada
+echo "Secuencia de aminoacidos traducida a partir de la secuencia de nucleotidos ingresada:"
+cat "$input_fasta"
+
+# Debe existir la carpeta llamada PROSITE en el directorio
 # Descarga el archivo PROSITE (prosite.dat)
 if [ ! -f prosite.dat ]; then
   wget ftp://ftp.expasy.org/databases/prosite/prosite.dat
@@ -32,4 +37,5 @@ patmatmotifs -full -sequence $input_fasta -outfile $output_file
 
 # Muestra los resultados del archivo creado y luego filtra y muestra solo los Motif generados con mi secuencia
 cat $output_file
+echo "Motifs encontrados con Prosite para nuestra proteina:"
 cat $output_file | grep Motif
